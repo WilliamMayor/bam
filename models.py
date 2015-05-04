69612992ef3e989300087d9b4c873fe470d63b49
William Mayor <mail@williammayor.co.uk>
2015-05-04 10:59:43 +0100

    Fixed weekday ordering

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Person(Base):
 
     def should_send_report(self):
         today = datetime.date.today()
-        today = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'][today.weekday()]
+        today = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'][today.weekday()]
         return today in self.report_frequency
 
     def should_send_alert(self, record):
